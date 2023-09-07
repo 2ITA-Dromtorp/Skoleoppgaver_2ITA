fa3ba5974c6342b47346b0e0c1f72b44ac622bb7
Joakim Husefest <joakimhus@viken.no>
2023-09-07 14:07:44 +0200

    started oppgave2

--- Grunnleggende Programmering/grunnleggende_programmering/src/App.js
+++ Grunnleggende Programmering/grunnleggende_programmering/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Oppgave1 from './Oppgave1';
+import Oppgave2 from './Oppgave2';
+
 
 function App() {
   
@@ -7,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-        <Oppgave1 />
+        <Oppgave2 />
 
       </header>
     </div>
