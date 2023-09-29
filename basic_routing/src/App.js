10f70005589e372a9224a44168b19ffc537eb83f
Joakim Husefest <joakimhus@viken.no>
2023-09-29 12:06:56 +0200

    klassekart

--- basic_routing/src/App.js
+++ basic_routing/src/App.js
@@ -9,20 +9,56 @@ function App() {
       <div className='learer'>
 
         <div className='person'>
-          
+          Lærer
         </div>
 
       </div>
       
       <div className='forste_rad'>
 
+      <div className='person'>
+          Martin
+        </div>
+        <div className='person'>
+          Mathias
+        </div>
+        <div className='person'>
+          Kevin
+        </div>
+        <div className='person'>
+          Andreas
+        </div>
+
       </div>
 
       <div className='andre_rad'>
+
+      <div className='person'>
+            Falk
+        </div>
+        <div className='person'>
+          Sander
+        </div>
+        <div className='person'>
+          Ylva
+        </div>
+        <div className='person'>
+          Vanessa
+        </div>
+        <div className='person'>
+          Chen
+        </div>
         
       </div>
 
       <div className='tredje_rad'>
+
+      <div className='person'>
+          Fridtjof
+        </div>
+        <div className='person'>
+          Luz
+        </div>
         
       </div>
 
