b7f3478e5ffb1963dfd3cee61a261848b2229733
Joakim Husefest <joakimhus@viken.no>
2023-09-29 10:48:43 +0200

    klassekart

---
+++ basic_routing/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='container'>
+
+      <div className='learer'>
+        
+      </div>
+      
+      <div className='forste_rad'>
+
+      </div>
+
+      <div className='andre_rad'>
+        
+      </div>
+
+      <div className='tredje_rad'>
+        
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
