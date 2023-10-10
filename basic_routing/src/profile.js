87e316789ff2d8910905f4b11ccae3a0d37fef63
Joakim Husefest <joakimhus@viken.no>
2023-10-10 14:52:14 +0200

    finished template to klassekart

--- basic_routing/src/profile.js
+++ basic_routing/src/profile.js
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
 
     const { profile } = useParams();
+    const navigate = useNavigate();
 
     console.log(profile);
 
     return (
-        <h1> Profilen til {profile} </h1>
+
+        <>
+            <h1> Profilen til {profile} </h1>
+            <button onClick={() => {navigate('/')}}>Til hovedmeny</button>
+        </>
     )
 }
