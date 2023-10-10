6ebbe203e073b5d17703c7f20bf8a2924b6f9886
Joakim Husefest <joakimhus@viken.no>
2023-10-10 15:42:53 +0200

    import json

--- basic_routing/src/profile.js
+++ basic_routing/src/profile.js
@@ -1,26 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import profileIMG from './images/profile-image.jpg'
+import json from './profiles'
 
 export default function Profile() {
 
     const { profile } = useParams();
     const navigate = useNavigate();
 
-    console.log(profile);
+    console.log(json.elever);
 
     return (
         <>
-        <div className="profile">
-        <div className="profilecard">
-        <h1> Profilen til {profile} </h1>
-        <img src={profileIMG}></img>
-            <p>Klasse: 10330023</p>
-            <p>E-mail: example@viken.no</p>
-            <p>Tlf.: 10330023</p>  
-        <button onClick={() => navigate("/")}>Til hovedmeny</button>
-        </div>
-        </div>
+            <div className="profile">
+                <div className="profilecard">
+                    <h1> {profile} </h1>
+                    <img src={profileIMG}></img>
+                        <p>Klasse: 10330023</p>
+                        <p>E-mail: example@viken.no</p>
+                        <p>Tlf.: 10330023</p>
+                    <button onClick={() => navigate("/")}>Til hovedmeny</button>
+                </div>
+            </div>
         </>
     )
 }
---
+++ basic_routing/src/profiles.js
@@ -0,0 +1,36 @@
+let json = {
+    "elever" : [
+        {
+          "navn" : "Chen",
+          "email" : "chen@viken.no",
+          "tlf": "92727212",
+          "klasse": "2ita"
+        },
+        {
+            "navn" : "Vanessa",
+            "email" : "vanessa@viken.no",
+            "tlf": "92727632",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "Ylva",
+            "email" : "ylva@viken.no",
+            "tlf": "92727655",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "andreas",
+            "email" : "andreas@viken.no",
+            "tlf": "92727555",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "falk",
+            "email" : "falk@viken.no",
+            "tlf": "92711555",
+            "klasse": "2ita"
+        }
+      ]
+}
+
+export default json;
