a81749ef670ab0d2901d12123d23315107857a97
Joakim Husefest <joakimhus@viken.no>
2023-10-12 13:54:48 +0200

    Almost finished with usestate version

--- basic_routing/src/profile.js
+++ basic_routing/src/profile.js
@@ -2,13 +2,27 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import profileIMG from './images/profile-image.jpg'
 import json from './profiles'
+import { useState } from 'react'
 
 export default function Profile() {
 
     const { profile } = useParams();
     const navigate = useNavigate();
+    console.log(profile);
 
-    console.log(json.elever);
+
+    const [student, setStudent] = useState({
+            "navn" : "Joakim",
+            "email" : "joakim@viken.no",
+            "tlf": "73737272",
+            "klasse": "lærer"
+    });
+
+    json.elever.map((item, index) => {
+        if (profile == item.navn) {
+            console.log(item);
+        }
+    })
 
     return (
         <>
@@ -16,9 +30,9 @@ export default function Profile() {
                 <div className="profilecard">
                     <h1> {profile} </h1>
                     <img src={profileIMG}></img>
-                        <p>Klasse: 10330023</p>
-                        <p>E-mail: example@viken.no</p>
-                        <p>Tlf.: 10330023</p>
+                        <p>Klasse: {profile.klasse}</p>
+                        <p>E-mail: {profile.email}</p>
+                        <p>Tlf.: {profile.tlf}</p>
                     <button onClick={() => navigate("/")}>Til hovedmeny</button>
                 </div>
             </div>
