718c9aec58261ff5b92e9931207a4f4e447db1a3
Joakim Husefest <joakimhus@viken.no>
2023-10-12 10:28:37 +0200

    started to work with json file

--- basic_routing/src/profiles.js
+++ basic_routing/src/profiles.js
@@ -3,13 +3,13 @@ let json = {
         {
           "navn" : "Chen",
           "email" : "chen@viken.no",
-          "tlf": "92727212",
+          "tlf": "92728212",
           "klasse": "2ita"
         },
         {
             "navn" : "Vanessa",
             "email" : "vanessa@viken.no",
-            "tlf": "92727632",
+            "tlf": "92127632",
             "klasse": "2ita"
         },
         {
@@ -19,15 +19,45 @@ let json = {
             "klasse": "2ita"
         },
         {
-            "navn" : "andreas",
+            "navn" : "Andreas",
             "email" : "andreas@viken.no",
             "tlf": "92727555",
             "klasse": "2ita"
         },
         {
-            "navn" : "falk",
+            "navn" : "Falk",
             "email" : "falk@viken.no",
             "tlf": "92711555",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "Martin",
+            "email" : "martin@viken.no",
+            "tlf": "92716555",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "Sander",
+            "email" : "sander@viken.no",
+            "tlf": "92741555",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "Kevin",
+            "email" : "kevin@viken.no",
+            "tlf": "92711555",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "Fridtjof",
+            "email" : "fridtjof@viken.no",
+            "tlf": "92221555",
+            "klasse": "2ita"
+        },
+        {
+            "navn" : "Luz",
+            "email" : "luz@viken.no",
+            "tlf": "92719555",
             "klasse": "2ita"
         }
       ]
