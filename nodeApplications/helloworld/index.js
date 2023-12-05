05248883443e70cc9fad46cba33a3bc9554db0f3
Joakim Tveit Husefest <joakimhus@viken.no>
2023-12-05 15:05:54 +0100

    started backend programming

--- nodeApplications/helloworld/index.js
+++ nodeApplications/helloworld/index.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'dromtorp'
+});
+
+connection.connect( function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+}
+);
+
+connection.query('SELECT * from elev', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
+ 
+connection.end();
 
 app.get('/', (request, response) => {
   response.send('Her kommer det sql data istedenfor denne teksten')
