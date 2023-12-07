11f82b6fcb2f1abca8400fdf480b9f9be773936f
Joakim Tveit Husefest <joakimhus@viken.no>
2023-12-07 13:33:18 +0100

    got it to work with database

--- nodeApplications/frontend/src/select.js
+++ nodeApplications/frontend/src/select.js
@@ -1,6 +1,15 @@
-
+import axios from "axios";
 
 export default function Select() {
+    
+const getCustomersData = () => {
+    axios
+        .get("http://localhost:3000/")
+        .then((data) => console.log(data))
+        .catch(error => console.log(error));
+};
+getCustomersData();
+
     return (
         <div>  Her kommer en tabell om databasen vår </div>
     )
--- nodeApplications/helloworld/index.js
+++ nodeApplications/helloworld/index.js
@@ -2,37 +2,57 @@ const express = require('express')
 const app = express()
 const port = 3000
 var mysql = require('mysql');
+var cors = require('cors')
+
+app.use(cors())
 
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: 'root',
+  user: 'johu',
+  password: 'dromtorp',
   database: 'dromtorp'
+});
+
+connection.connect(function(err) {
+
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
 });
 
 
 app.get('/', (request, response) => {
 
-  connection.connect( function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-  }
-  );
-
-  connection.query('SELECT * from elev', function (error, results, fields) {
+  connection.query('SELECT * FROM elev', function (error, results, fields) {
     if (error) throw error;
-      response.send(JSON.stringify(results))
+    response.send(JSON.stringify(results));
   });
 
-  connection.end();
-
-
+  
+  
 })
+
+/*app.get("/updateuser/:newhobby/:id", (request, response) => {
+  
+  let newhobby = request.params.newhobby;
+  let id = request.params.id;
+  console.log(newhobby);
+  let sqlquery = 'UPDATE elev SET hobby=? WHERE ElevID=?';
+
+  connection.query(sqlquery, [newhobby, id], function (error, results, fields) {
+    if (error) throw error;
+    response.send(JSON.stringify(results));
+  });
+
+  response.send('If This works, great!');
+  
+})*/
 
 app.listen(port, () => {
-  console.log(`Joakims app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
+
